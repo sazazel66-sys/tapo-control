5821c83965118f935a64495fc5ad5d8088cade43
sazazel66-sys <sazazel66@gmail.com>
2026-04-13 18:30:03 +0200

    Update tapo.js

--- api/tapo.js
+++ api/tapo.js
@@ -2,7 +2,6 @@ export default async function handler(req, res) {
   try {
     const action = req.query.action || "on";
     
-    // Login en servidor EU
     const loginResp = await fetch("https://eu-wap.tplinkcloud.com", {
       method: "POST",
       headers: {"Content-Type": "application/json"},
@@ -18,8 +17,25 @@ export default async function handler(req, res) {
     });
     
     const loginData = await loginResp.json();
-    res.json({loginData, action});
+    const token = encodeURIComponent(loginData.result.token);
+    const state = action === "on" ? 1 : 0;
     
+    const resp = await fetch("https://eu-wap.tplinkcloud.com?token=" + token, {
+      method: "POST",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify({
+        method: "passthrough",
+        params: {
+          deviceId: "80227856C64550113A3799EBF9E2B7C5252BEEC6",
+          requestData: JSON.stringify({
+            system: {set_relay_state: {state: state}}
+          })
+        }
+      })
+    });
+    
+    const data = await resp.json();
+    res.json(data);
   } catch(e) {
     res.status(500).json({error: e.message});
   }
