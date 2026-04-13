b6b3e23e32f01e7c15d1cdc38fda2cb534544cd9
sazazel66-sys <sazazel66@gmail.com>
2026-04-13 18:28:43 +0200

    Update tapo.js

--- api/tapo.js
+++ api/tapo.js
@@ -2,7 +2,8 @@ export default async function handler(req, res) {
   try {
     const action = req.query.action || "on";
     
-    const loginResp = await fetch("https://wap.tplinkcloud.com", {
+    // Login en servidor EU
+    const loginResp = await fetch("https://eu-wap.tplinkcloud.com", {
       method: "POST",
       headers: {"Content-Type": "application/json"},
       body: JSON.stringify({
@@ -17,26 +18,8 @@ export default async function handler(req, res) {
     });
     
     const loginData = await loginResp.json();
-    const token = encodeURIComponent(loginData.result.token);
-    const device_on = action === "on" ? true : false;
+    res.json({loginData, action});
     
-    const resp = await fetch("https://eu-wap.tplinkcloud.com?token=" + token, {
-      method: "POST",
-      headers: {"Content-Type": "application/json"},
-      body: JSON.stringify({
-        method: "passthrough",
-        params: {
-          deviceId: "80227856C64550113A3799EBF9E2B7C5252BEEC6",
-          requestData: JSON.stringify({
-            method: "set_device_info",
-            params: {device_on: device_on}
-          })
-        }
-      })
-    });
-    
-    const data = await resp.json();
-    res.json(data);
   } catch(e) {
     res.status(500).json({error: e.message});
   }
