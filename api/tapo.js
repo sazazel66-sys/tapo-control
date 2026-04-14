2f98065ed6d1e3dd4036c736ad53e026bc36bc25
sazazel66-sys <sazazel66@gmail.com>
2026-04-14 12:50:18 +0200

    Update tapo.js

--- api/tapo.js
+++ api/tapo.js
@@ -1,39 +1,17 @@
 export default async function handler(req, res) {
   try {
-    const action = req.query.action || "on";
-    
     const loginResp = await fetch("https://eu-wap.tplinkcloud.com", {
       method: "POST",
       headers: {"Content-Type": "application/json"},
-      body: JSON.stringify({
-        method: "login",
-        params: {
-          appType: "Tapo_Android",
-          cloudUserName: "navepresenti@gmail.com",
-          cloudPassword: "Presenti1",
-          terminalUUID: "12345678-1234-1234-1234-123456789abc"
-        }
-      })
+      body: JSON.stringify({method: "login",params: {appType: "Tapo_Android",cloudUserName: "navepresenti@gmail.com",cloudPassword: "Presenti1",terminalUUID: "12345678-1234-1234-1234-123456789abc"}})
     });
-    
     const loginData = await loginResp.json();
     const token = encodeURIComponent(loginData.result.token);
-    const state = action === "on" ? 1 : 0;
-    
     const resp = await fetch("https://eu-wap.tplinkcloud.com?token=" + token, {
       method: "POST",
       headers: {"Content-Type": "application/json"},
-      body: JSON.stringify({
-        method: "passthrough",
-        params: {
-          deviceId: "80227856C64550113A3799EBF9E2B7C5252BEEC6",
-          requestData: JSON.stringify({
-            system: {set_relay_state: {state: state}}
-          })
-        }
-      })
+      body: JSON.stringify({method: "getDeviceList"})
     });
-    
     const data = await resp.json();
     res.json(data);
   } catch(e) {
